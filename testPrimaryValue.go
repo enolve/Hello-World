1b324ca9e04ae364e7a1c65a06bc3aba088f7dea
enolve <604362179@qq.com>
2016-06-06 13:04:33 +0800

    Update testPrimaryValue.go
    从《GO语言编程》上down下来，花了一段时间才看懂，以此记录

--- testPrimaryValue.go
+++ testPrimaryValue.go
@@ -7,8 +7,12 @@ import (
 	"strconv"
 )
 
-var goal int
+var goal int //求此数以内的素数
 
+/*
+	第一个协程：将不能被2整数的数添加到第二个协程的channel中，第一个数为基数(3)
+	第二：将不能被3整除的数添加到下个协程中，以此循环
+*/
 func primetask(c chan int) {
 	p := <-c
 	if p > goal {
@@ -45,6 +49,6 @@ func main() {
 	c := make(chan int)
 	go primetask(c)
 	for i := 2; ; i++ {
-		c <- i
+		c <- i  
 	}
 }
